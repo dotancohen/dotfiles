09efad64ec8d84490fffe3713a33bfe2bc1af8bd
Dotan Cohen <github-com-2010.04@dotancohen.com>
2018-03-01 10:19:00 +0200

    Add SVN Search file

---
+++ .bin/svnsearch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find SVN revision by text added or removed
+# http://stackoverflow.com/a/2742851/343302
+
+file="$1"
+REVISIONS=`svn log $file -q --stop-on-copy |grep "^r" | cut -d"r" -f2 | cut -d" " -f1`
+for rev in $REVISIONS; do
+    prevRev=$(($rev-1))
+    difftext=`svn diff --old=$file@$prevRev --new=$file@$rev | tr -s " " | grep -v " -\ \- " | grep -e "$2"`
+    if [ -n "$difftext" ]; then
+        echo "$rev: $difftext"
+    fi
+done
